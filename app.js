d0bf37aebfca7a959105116bd82b92e1a978fc97
goncharuk.valera <goncharukvalera@gmail.com>
2020-11-25 19:27:44 +0200

    Connection to db

--- app.js
+++ app.js
@@ -5,4 +5,18 @@ const mongoose = require('mongoose')
 const app = express()
 
 const PORT = config.get('port') || 5000
-app.listen(PORT, () => console.log(`App has started on port ${PORT}`))
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`App has started on port ${PORT}`))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+start()
