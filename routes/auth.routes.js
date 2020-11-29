772574e4e611fa1772d4a0431edafad8fdd333be
goncharukvalera <goncharukvalera@gmail.com>
2020-11-29 21:02:22 +0200

    Auth routing

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,26 @@
-const {Router} = require('express');
+const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 const router = Router()
 
+// /api/auth/register
+router.post('/register', async (req, resp) => {
+    try {
+        const {email, password} = req.body
+        const candidate = await User.findOne({email})
+        if (candidate) {
+            return resp.status(400).json({message: 'User with this email already exist'})
+        }
+        const hashedPassword = await bcrypt.hash(password, 'VH')
+        const user = new User({email, password: hashedPassword})
+        await user.save()
+        resp.status(201).json({message: 'User was successfully created'})
+    } catch (e) {
+        resp.status(500).json({message: 'Something went wrong'})
+    }
+})
+// /api/auth/login
+router.post('/login', async (req, resp) => {
+
+})
 module.exports = router
