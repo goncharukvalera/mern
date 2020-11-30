0b295290e9d947b14ae453b9b792069f89a5c0f4
goncharuk.valera <goncharukvalera@gmail.com>
2020-11-30 17:59:41 +0200

    Initialize React

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'materialize-css'
+function App() {
+  return (
+      <div className="container">
+          <h1>Hello MERN</h1>
+      </div>
+  )
+}
+
+export default App
