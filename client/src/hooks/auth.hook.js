5eba97df922bd280ede5c68d88002c0dc183a115
goncharukvalera <goncharukvalera@gmail.com>
2020-12-16 22:40:15 +0200

    Auth hook (end)

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -1,11 +1,26 @@
-import {useState, useCallback} from 'react'
+import {useState, useCallback, useEffect} from 'react'
+const storageName = 'userData'
 
 export const useAuth = () => {
     const [token, setToken] = useState(null)
     const [userId, setUserId] = useState(null)
 
-    const login = useCallback(() => {}, [])
-    const logout = useCallback(() => {}, [])
+    const login = useCallback((jwtToken, id) => {
+        setToken(jwtToken)
+        setUserId(id)
+        localStorage.setItem(storageName, JSON.stringify({userId, token}))
+    }, [])
+    const logout = useCallback(() => {
+        setToken(null)
+        setUserId(null)
+        localStorage.removeItem(storageName)
+    }, [])
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem(storageName))
+        if (data && data.token && data.userId) {
+            login(data.token, data.userId)
+        }
+    }, [login])
 
     return {login, logout}
 }
