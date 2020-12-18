49e2d3ddf6c371a6ce9e0bce33e67a1e6a4c30b4
goncharukvalera <goncharukvalera@gmail.com>
2020-12-19 00:14:43 +0200

    Link logic - finish + shortid library

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,13 +1,25 @@
 const {Router} = require('express')
 const Link = require('../models/Link')
 const config = require('config')
+const shortid = require('shortid')
 const auth = require('../middleware/auth.middleware')
 const router = Router()
 
-router.post('/generate', async (req, resp) => {
+router.post('/generate', auth, async (req, resp) => {
     try {
         const baseUrl = config.get('baseUrl')
         const {from} = req.body
+        const code = shortid.generate()
+        const existing = await Link.findOne({from})
+        if (existing) {
+            return resp.json({link: existing})
+        }
+        const to = baseUrl + '/t/' + code
+        const link = new Link({
+            code, to, from, owner: req.user.userId
+        })
+        await link.save()
+        resp.status(201).json({link})
     } catch (e) {
         resp.status(500).json({message: 'Something went wrong'})
     }
@@ -20,7 +32,7 @@ router.get('/', auth, async (req, resp) => {
         resp.status(500).json({message: 'Something went wrong'})
     }
 })
-router.get('/:id', async (req, resp) => {
+router.get('/:id', auth, async (req, resp) => {
     try {
         const link = await Link.findById(req.params.id)
         resp.json(link)
