4a802082b167d804dda05990f99fcc25001a2d10
goncharukvalera <goncharukvalera@gmail.com>
2020-12-20 22:26:09 +0200

    LinkCard

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export const LinkCard = ({link}) => {
-
+    return (
+        <>
+            <h2>Link</h2>
+            <p>Your link: <a href={link.to} target="_blank" rel="noreferrer noopener">{link.to}</a></p>
+            <p>From: <a href={link.from} target="_blank" rel="noreferrer noopener">{link.from}</a></p>
+            <p>Click by link: <b>{link.clicks}</b></p>
+            <p>Created date: <b>{new Date(link.date).toLocaleDateString()}</b></p>
+        </>
+    )
 }
