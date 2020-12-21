d1f0652a369df746b12583fbd1b004044b1939b6
goncharuk.valera <goncharukvalera@gmail.com>
2020-12-21 20:20:06 +0200

    Logic on server

---
+++ routes/redirect.routes.js
@@ -0,0 +1,18 @@
+const {Router} = require('express')
+const Link = require('../models/Link')
+const router = Router()
+
+router.get('/:code', async (req, resp) => {
+    try {
+        const link = await Link.findOne({code: req.params.code})
+        if (link) {
+            link.clicks++
+            link.save()
+            return resp.redirect(link.from)
+        }
+        resp.status(404).json({message: 'Link not found'})
+    } catch (e) {
+        resp.status(500).json({message: 'Something went wrong'})
+    }
+})
+module.exports = router
